4f2597af65597567fe9f03c5ee5a546dec03743f
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2020-07-13 21:53:35 +0100

    +placeholder API endpoints

--- index.js
+++ index.js
@@ -5,4 +5,16 @@ const PORT = process.argv[2] || 3000
 
 APP.use(EXPRESS.static('./static'));
 
+APP.get('/api/getqueue', (req, res) => {
+    res.send('this endpoint is not implimented yet');
+});
+
+APP.post('/api/addtoqueue', (req, res) => {
+    res.send('this endpoint is not implimented yet');
+});
+
+APP.delete('/api/removefromqueue', (req, res) => {
+    res.send('this endpoint is not implimented yet');
+});
+
 APP.listen(PORT);
