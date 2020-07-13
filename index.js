4c5301fa2420b8ab6e25a4ee846b7dd285339244
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2020-07-13 21:49:52 +0100

    +basic static server

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const FS = require('fs').promises;
 const EXPRESS = require('express');
 const APP = EXPRESS();
 const PORT = process.argv[2] || 3000
+
+APP.use(EXPRESS.static('./static'));
+
+APP.listen(PORT);
