07b50c323f2bbec9a57b3e4b023a7d3751a1a91c
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2020-07-14 08:27:34 +0100

    +queue item handler class

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,3 +1,21 @@
+class QueueItemHandler {
+    constructor(elementTemplate, outputElement, queueNumber) {
+        this.element = outputElement.appendChild(elementTemplate.cloneNode(true));
+        this.number = queueNumber;
+
+        this.element.innerHTML = this.element.innerHTML.replace(/{{queueNumber}}/, this.number);
+
+        this.element.querySelector('.queueRemoveButton').addEventListener('click', () => {
+            this.remove();
+        })
+    }
+
+    remove() {
+        this.element.parentNode.removeChild(this.element);
+        fetch(`/api/removefromqueue?number=${this.number}`, { method: 'DELETE' });
+    }
+}
+
 const QUEUE_HOLDER = document.querySelector('#queueHolder');
 const QUEUE_ITEM_TEMPLATE = document.querySelector('#queueItemTemplate').content.firstElementChild;
 const QUEUE_NUMBER_ENTRY = document.querySelector('#queueAdderNumber')
