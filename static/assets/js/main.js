3da5639096bbc191868d901981c34ac79c5822e3
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2020-07-14 08:21:12 +0100

    +setup global variables

---
+++ static/assets/js/main.js
@@ -0,0 +1,6 @@
+const QUEUE_HOLDER = document.querySelector('#queueHolder');
+const QUEUE_ITEM_TEMPLATE = document.querySelector('#queueItemTemplate').content.firstElementChild;
+const QUEUE_NUMBER_ENTRY = document.querySelector('#queueAdderNumber')
+const QUEUE_NUMBER_SUBMIT = document.querySelector('#queueAdderAddButton');
+const QUEUE_ITEM_HANDLERS = [];
+
